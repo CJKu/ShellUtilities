9dd504063c5825d24b07b69244d075915d7979e9
CJKu <cjcool.tw@gmail.com>
2014-09-19 14:41:17 +0800

    fix little period

--- silk_log.sh
+++ silk_log.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# A tool to parse gecko log and retrieve silk log
+#
 
 if [ -z $1 ]; then
   echo "Input filename missed."
   exit 1
 fi
 
+# The name of source log file
 IN=$1
+# The name of output field file
 # default output filename is "output.txt"
 OUT=${2:-output.txt}
 
@@ -13,7 +17,7 @@ echo "Generate and put field data into \"${OUT}\""
 
 # sample name
 LOG_NAME="Silk input resample"
-SILK_LOG_PATTERN="s/.*${LOG_NAME} *, *\([0-9]*\) *, *\([0-9]*\)/\1 \2/"
+SILK_LOG_PATTERN="s/.*${LOG_NAME} *, *\([0-9]*\) *, *\([.0-9]*\).*/\1 \2/"
 
 # Convert log string into two fields: frame number + distance
 # For example,
@@ -21,3 +25,5 @@ SILK_LOG_PATTERN="s/.*${LOG_NAME} *, *\([0-9]*\) *, *\([0-9]*\)/\1 \2/"
 # tunrs into
 # "196827 0.030"
 sed -n "/${LOG_NAME}/p" "${1}" | sed "${SILK_LOG_PATTERN}" > ${OUT}
+
+echo "Done..."
