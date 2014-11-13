1b605e5e7c15c0c840081c5763b1c7eee48dfcd2
CJKu <cjcool.tw@gmail.com>
2014-11-13 17:12:13 +0800

    add gitignore

--- mkid-all.sh
+++ mkid-all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 SOURCES=(
-  ${GAIA_PATH-'../gaia/'}
-  ${GECKO_PATH-'../gecko-dev/'}
+  ${GAIA_PATH-"$HOME/repository/gaia/"}
+  ${GECKO_PATH-"$HOME/repository/gecko-dev/"}
 )
 
 for path in ${SOURCES[@]}
 do
   echo "mkid $path"
+  cd $path
   mkid -m idmap.txt "$path" # 2>/dev/null 1>/dev/null
 done
